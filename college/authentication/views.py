a60c778a8cacc8aee52f86d70699527450ecdcc3
MaksimLion <maxim226356@mail.ru>
2019-03-20 02:57:08 +0300

    testing pdf detail

--- college/authentication/views.py
+++ college/authentication/views.py
@@ -182,7 +182,7 @@ def lab_detail(request,lab_id):
     # with open(lab.read_file, 'rb') as pdf:
     #     text = pdf.readline()
     context = {
-        'text' : chr(lab.read_file),
+        'text' : lab.read_file.readline()
     }
     return render(request, 'lab_detail.html', context)
 
