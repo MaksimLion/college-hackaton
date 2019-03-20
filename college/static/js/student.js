ee7c1365a8db1b449871ed115c97e0609d1816c0
MaksimL <maks.war@ya.ru>
2019-03-20 04:27:10 +0300

    added front-end labs, reports pages

---
+++ college/static/js/student.js
@@ -0,0 +1,9 @@
+function lookForMyReports() {
+    event.preventDefault()
+    document.location.href = "/my_account/statistics"
+}
+
+function lookForLabs() {
+    event.preventDefault()
+    document.location.href = "/labs"
+}
